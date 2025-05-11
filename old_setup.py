e63b2220cf680e119df31f4e220ef2a7d8dd0cef
Jack Dempsey <jade8185@colorado.edu>
2025-05-11 19:38:21 -0400

    push to git

--- old_setup.py
+++ old_setup.py
@@ -5,11 +5,13 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-    ],
+    url="https://github.com/SandyKeeps/py_sandbox",
+    # install_requires=[
+    # ],
     entry_points={
         "console_scripts": [
             "py_sandbox = py_sandbox.cli:main",
         ],
     },
+    python_requires=">=3.11",
 )
